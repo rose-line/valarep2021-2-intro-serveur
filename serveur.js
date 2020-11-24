88e3fca8a2dfaccba96a7b07152d936dc68220b2
rose-line <patrice.gahide@gmail.com>
2020-11-24 11:01:33 +0100

    Serveur avec Express

--- serveur.js
+++ serveur.js
@@ -1,14 +1,40 @@
-let http = require('http');
+let express = require('express');
+let serveur = express();
 
-let serveur = http.createServer((req, res) => {
-  //console.log(req);
-  if (req.url === '/') {
-    res.end('Bienvenue sur notre site !');
-  } else if (req.url === '/contact') {
-    res.end("Contactez-nous a l'adresse toto@machin.com");
+serveur.use(express.urlencoded({ extended: false }));
+
+serveur.get('/', (req, res) => {
+  //console.log(`requête sur '/'`);
+  res.send(`
+    <form action="/reponse" method="POST">
+      <p>Combien font 2 + 2 ?</p>
+      <input name="reponseDonnee" autocomplete="off">
+      <button>Envoyer</button>
+    </form>
+  `);
+});
+
+serveur.post('/reponse', function (req, res) {
+  //res.send('Bien reçu');
+  // recuperer la valeur du formulaire
+  //console.log(req.body.reponseDonnee);
+  if (req.body.reponseDonnee === '4') {
+    res.send(`Bravo ! <br>
+    <a href='/'>Retour à l'accueil</a>`);
   } else {
-    res.end("La page n'existe pas...");
+    res.send(`Perdu ! <br>
+    <a href='/'>Retour à l'accueil</a>`);
   }
 });
+
+serveur.get('/reponse', (req, res) => {
+  res.send('Êtes-vous perdu ?');
+})
+
+
+// camelCase => premiereLettreEnMinuscule
+// PascalCase => PremiereLettreEnMajuscule
+// snake_case => mots_separes_par_des_tirets
+
 serveur.listen(3000);
-console.log('serveur démarré - écoute sur port 3000...');
+console.log('ecoute sur port 3000...');
